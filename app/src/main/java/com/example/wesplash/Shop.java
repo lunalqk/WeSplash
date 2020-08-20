8f456bed48d7254aff7f6052e235be9854c17dde
lunalqk <lunal22@yorkhouse.ca>
2020-08-19 20:42:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/wesplash/Shop.java
@@ -0,0 +1,55 @@
+package com.example.wesplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Shop extends AppCompatActivity {
+    private TextView Activity;
+    private TextView Tips;
+    private TextView Pairing;
+    private TextView Blog;
+    private TextView Shop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+
+        Activity = findViewById(R.id.Activity);
+        Tips = findViewById(R.id.Tips);
+        Pairing = findViewById(R.id.Pairing);
+        Blog = findViewById(R.id.Blog);
+        Shop = findViewById(R.id.Shop);
+
+        Tips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Pairing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Blog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
